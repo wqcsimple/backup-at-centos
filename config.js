b4d448c9d5eb79e48bc61e46eb898a2542b02d1d
whis <wqcsimple@foxmail.com>
2017-11-10 14:43:40 +0800

    bak

--- config.js
+++ config.js
@@ -5,13 +5,13 @@
 
 // 备份的目录
 module.exports.UPLOAD_FOLDER_LIST = [
-    '/Users/whis/Downloads/mysql'
+    ''
 ];
 
 
 // 七牛配置信息
-module.exports.ACCESS_KEY = "xqupAU168xYM7YaYKmbeK1T83jQTZGYSm_ZKFwBa";
-module.exports.SECRET_KEY = "5w4R06-MMu7V2wsMoyjO8fXMHQjDlFXnVDmAqhQt";
+module.exports.ACCESS_KEY = "";
+module.exports.SECRET_KEY = "";
 
 module.exports.COS_BUCKET = "disk01";
 module.exports.COS_KEY = "file/";
